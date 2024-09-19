d24d04103e686fa2d1fcd224a3cee1ff96063c99
Leandro Melo <leo1309melo@hotmail.com>
2024-09-19 16:23:38 -0300

    feat: config redirects and middlewares

--- app.vue
+++ app.vue
@@ -1,17 +1,33 @@
 <template>
-  <v-app>
-    <div class="root-class"></div>
-  </v-app>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
 </template>
 
-<style>
+<script setup lang="ts">
+const router = useRouter();
+
+const nav = [
+  { label: "Home", to: "/" },
+  { label: "Login", to: "/login" },
+  // { label: 'Parent (index)', to: '/parent' },
+  // { label: 'Parent (b)', to: '/parent/b' },
+  // { label: 'Keyed child', onClick: () => router.push(`/parent/reload-${(Math.random() * 100).toFixed()}`) },
+  // { label: 'Non-Keyed child', onClick: () => router.push(`/parent/static-${(Math.random() * 100).toFixed()}`) },
+];
+
+</script>
+
+<style lang="scss">
 body,
 html {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
   font-size: 16px;
-  background: #f5f5f5;
 }
 
 html,
@@ -21,5 +37,6 @@ body,
 .root-class {
   height: 100% !important;
   width: 100% !important;
+  background: $secondary;
 }
 </style>
